1f0da9edc90b603f073dedfae834338f5048a252
Yodae <yodae.lim@gmail.com>
2023-06-18 22:57:05 -0700

    Advert and friendlistwidget

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -4,6 +4,8 @@ import UserWidget from "scenes/widgets/UserWidget";
 import { useSelector } from "react-redux";
 import MyPostWidget from "scenes/widgets/MyPostWidget";
 import PostsWidget from "scenes/widgets/PostsWidget";
+import AdvertWidget from "scenes/widgets/AdvertWidget";
+import FriendListWidget from "scenes/widgets/FriendListWidget";
 
 const HomePage = () =>{
     const isNonMobileScreens = useMediaQuery("(min-width: 100px)");
@@ -31,7 +33,10 @@ const HomePage = () =>{
                 </Box>
                 {isNonMobileScreens && (
                     <Box flexBasis="26%">
-
+                        <AdvertWidget/>
+                        <Box m="2rem 0">
+                            <FriendListWidget userId={_id}/>
+                        </Box>
                     </Box>
                 )}
             </Box>
